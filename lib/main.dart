500267136b21a0843a3212a37b9dee33a97b0958
Saeed Noshadi <saeednoshadi89@gmail.com>
2023-05-04 17:07:47 +0330

    implement hive db and search box in To Do List screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todo_app/db/model/task.dart';
 
-void main() {
+const taskBoxName = 'tasks';
+
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(TaskAdapter());
+  Hive.registerAdapter(PriorityAdapter());
+  await Hive.openBox<Task>(taskBoxName);
+
   runApp(const MyApp());
 }
+
+const primaryColor = Color(0xff794CFF);
+const primaryContainerColor = Color(0xff5C0AFF);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,106 +23,158 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const primaryTextColor = Color(0xff1D2830);
+    const secondaryTextColor = Color(0xffAFBED0);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        inputDecorationTheme: const InputDecorationTheme(
+            labelStyle: TextStyle(color: secondaryTextColor),
+            iconColor: secondaryTextColor),
+        colorScheme: const ColorScheme.light(
+            primary: primaryColor,
+            primaryContainer: primaryContainerColor,
+            background: Color(0xffF3F5F8),
+            onSurface: primaryTextColor,
+            onPrimary: Colors.white,
+            onBackground: secondaryTextColor,
+            secondary: primaryColor,
+            onSecondary: Colors.white),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TaskListScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class TaskListScreen extends StatelessWidget {
+  const TaskListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final box = Hive.box<Task>(taskBoxName);
+    final themeData = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => EditTaskScreen()));
+          },
+          label: const Text('Add New Task')),
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Container(
+              height: 122,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    themeData.colorScheme.primary,
+                    themeData.colorScheme.primaryContainer,
+                  ],
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'To Do list',
+                          style: themeData.textTheme.titleLarge
+                              ?.apply(color: themeData.colorScheme.onPrimary),
+                        ),
+                        Icon(
+                          CupertinoIcons.share,
+                          color: themeData.colorScheme.onPrimary,
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      height: 45.0,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(19.0),
+                          color: themeData.colorScheme.onPrimary,
+                          boxShadow: [
+                            BoxShadow(color: Colors.black.withOpacity(1))
+                          ]),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(CupertinoIcons.search),
+                            label: Text('Search tasks...'),
+                            border: InputBorder.none),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Expanded(
+              child: ValueListenableBuilder<Box<Task>>(
+                valueListenable: box.listenable(),
+                builder: (context, values, child) {
+                  return ListView.builder(
+                      itemCount: values.values.length,
+                      itemBuilder: (context, index) {
+                        final Task task = box.values.toList()[index];
+                        return Container(
+                          child: Text(
+                            task.name,
+                            style: const TextStyle(fontSize: 24),
+                          ),
+                        );
+                      });
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class EditTaskScreen extends StatelessWidget {
+  final TextEditingController _controller = TextEditingController();
+
+  EditTaskScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit task'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            final task = Task();
+            task.name = _controller.text;
+            task.priority = Priority.low;
+            if (task.isInBox) {
+              task.save();
+            } else {
+              final Box<Task> box = Hive.box(taskBoxName);
+              box.add(task);
+            }
+            Navigator.of(context).pop();
+          },
+          label: const Text('Save Changes')),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration:
+                const InputDecoration(label: Text('Add a task for today...')),
+          )
+        ],
+      ),
     );
   }
 }
